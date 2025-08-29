f51a3d7748f1a269b99d02e11443dfeb772298cf
LucaRuggeri5 <luca.ruggeri5@gmail.com>
2025-08-29 16:58:25 +0200

    esercizio 8 finalizzato

--- script.js
+++ script.js
@@ -40,4 +40,5 @@ console.log("Lewis si trova alla posizione numero:", lewisIndex);
 
 // 8. Verifica se l'array teachers è vuoto
 // e salva il risultato nella variabile isTeachersEmpty
-const isTeachersEmpty = null;
+const isTeachersEmpty = teachers.length === 0;
+console.log("L'array degli insegnanti sono vuoti?", isTeachersEmpty);
